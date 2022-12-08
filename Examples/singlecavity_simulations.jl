8d22a22ed27b980230d48419f392a5d57d030757
mabuni1998 <56378038+mabuni1998@users.noreply.github.com>
2022-12-08 17:15:38 +0100

    Create singlecavity_simulations.jl

---
+++ Examples/singlecavity_simulations.jl
@@ -0,0 +1,23 @@
+using Parameters
+using NumericalIntegration
+using DifferentialEquations
+
+@with_kw mutable struct parameters
+    γ::Float64 = 1.0
+    δ::Float64 = 0.0
+    σ::Float64 = 1.0
+    t0::Float64 = 25.0
+    N_cut::Int64 = 1.0
+    times = 0.0:0.1:50.0
+    x3::Float64 = 0.0
+    δb::Float64 = 0.0
+    Λn::Float64 = 0.0
+end
+
+function solve_differentialeq(param::parameters,ξfun)
+    @unpack γ,δ,σ,t0,N_cut,times = param
+    p = [δ,γ]
+    f(ψ,p,t) = complex(-(im*p[1]+p[2]/2)*ψ + sqrt(p[2])*ξfun(t,σ,t0))
+    prob = ODEProblem(f,sqrt(p[2])*ξfun(times[1],σ,t0),(times[1],times[end]),p)
+    solve(prob, Tsit5(), reltol=1e-8, abstol=1e-8)
+end

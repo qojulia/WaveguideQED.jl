25cae2eb60c3f47afa6df6413d919d823bc3c053
Stefan Krastanov <stefan@krastanov.org>
2023-02-07 11:14:29 -0500

    start docs

---
+++ docs/make.jl
@@ -0,0 +1,28 @@
+push!(LOAD_PATH,"../src/")
+
+using Documenter
+using DocumenterCitations
+using CavityWaveguide
+
+DocMeta.setdocmeta!(CavityWaveguide, :DocTestSetup, :(using CavityWaveguide); recursive=true)
+
+bib = CitationBibliography(joinpath(@__DIR__,"src/references.bib"))
+
+makedocs(
+bib,
+doctest = false,
+clean = true,
+sitename = "CavityWaveguide.jl",
+format = Documenter.HTML(),
+modules = [CavityWaveguide],
+authors = "Matias Bundgaard-Nielsen",
+pages = [
+"CavityWaveguide.jl" => "index.md",
+"API" => "API.md",
+"References" => "references.md",
+]
+)
+
+deploydocs(
+    repo = "github.com/mabuni1998/CavityWaveguide.git"
+)

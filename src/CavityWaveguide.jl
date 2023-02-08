e40beb6775c56bbf8c435896e23f8b9fcc7144a0
mabuni1998 <mabuni1998@gmail.com>
2023-02-08 16:58:59 -0600

    Created documentation workflow

--- src/CavityWaveguide.jl
+++ src/CavityWaveguide.jl
@@ -3,7 +3,7 @@ module CavityWaveguide
 using QuantumOptics, LinearAlgebra, UnsafeArrays
 
 export view_twophoton,get_cwbasis,get_woper,get_wdoper,WaveguideBasis,view_onephoton,WaveguideOperator, waveguide_evolution, onephoton, twophoton,
-TwophotonView,get_waveguidetimeindex,get_nsteps,view_waveguide,emission,absorption,TwophotonTimestepView,get_waveguidetimeindex
+TwophotonView,get_waveguidetimeindex,get_nsteps,view_waveguide,emission,absorption,get_waveguidetimeindex,TwophotonTimestepView,get_waveguide_location
 
 include("view.jl")
 include("basis.jl")
--- src/basis.jl
+++ src/basis.jl
@@ -1,5 +1,3 @@
-#Custom basis for Waveguide
-
 """
     WaveguideBasis(N, times)
 
@@ -24,15 +22,17 @@ mutable struct WaveguideBasis{P} <: QuantumOptics.Basis
     end
 end
 
+
 """
     onephoton(b::WaveguideBasis,ξ::Function,times,args...,norm=True)
     onephoton(b::WaveguideBasis,ξvec;norm=true)
 
-Create a onephoton wavepacket of the form ``W^†(ξ) |0⟩ = \\int_{t_0}^{t_{end}} dt  ξ(t) w^†(t) |0⟩``. Here ``t_0=0`` and ``t_{end}`` is determined by the waveguide Basis BW.
-ξ is a function that takes 
+Create a onephoton wavepacket of the form ``W^†(ξ) |0⟩ = \\int_{t_0}^{t_{end}} dt  ξ(t) w^†(t) |0⟩``. Here ``t_0=0`` and ``t_{end}`` is determined by [`WaveguideBasis`](@ref).
+ξ is a function evaluated as `ξ.(times,args...)`.
+ξvec is a vector of length: `b.nsteps`.
+If `norm==true` the state is normalized through `normalize!`.
 
 """
-
 function onephoton(b::WaveguideBasis,ξ::Function,times,args...; norm=true)
     state = Ket(b)
     view = view_onephoton(state)
@@ -42,7 +42,6 @@ function onephoton(b::WaveguideBasis,ξ::Function,times,args...; norm=true)
     end
     return state
 end
-
 function onephoton(b::WaveguideBasis,ξvec;norm=true)
     state = Ket(b)
     view = view_onephoton(state)
@@ -53,6 +52,29 @@ function onephoton(b::WaveguideBasis,ξvec;norm=true)
     return state
 end
 
+
+"""
+    twophoton(b::WaveguideBasis,ξ::Function,times,args...,norm=True)
+    twophoton(b::WaveguideBasis,ξvec::Matrix;norm=true)
+
+Create a twophoton wavepacket of the form ``\\int_{t_0}^{t_{end}} dt' \\int_{t_0}^{t_{end}} dt  ξ(t,t') w^†(t)w^†(t') |0⟩``. Here ``t_0=0`` and ``t_{end}`` is determined by [`WaveguideBasis`](@ref).
+ξ is a function evaluated as `ξ(t1,t2,args...)`. ξvec is a matrix of dimension: `(b.nsteps,b.nsteps)`, where `ξvec[i,j] = ξ(times[i],times[j])`, where times is defined in [`WaveguideBasis`](@ref).
+
+"""
+function twophoton(b::WaveguideBasis,ξ::Function,times,args...;norm=true)
+    state = Ket(b)
+    nsteps = get_nsteps(b)
+    viewed_data = TwophotonView(state.data,nsteps)
+    for i in 1:nsteps
+        for j in i:nsteps
+            viewed_data[i,j] = ξ(times[i],times[j],args...)
+        end
+    end
+    if norm
+        normalize!(state)
+    end
+    return state
+end
 function twophoton(b::WaveguideBasis,ξ::Matrix;norm=true)
     state = Ket(b)
     nsteps = get_nsteps(b)
@@ -68,59 +90,73 @@ function twophoton(b::WaveguideBasis,ξ::Matrix;norm=true)
     return state
 end
 
-function twophoton(b::WaveguideBasis,ξ::Function,times,args...;norm=true)
-    state = Ket(b)
-    nsteps = get_nsteps(b)
-    viewed_data = TwophotonView(state.data,nsteps)
-    for i in 1:nsteps
-        for j in i:nsteps
-            viewed_data[i,j] = ξ(times[i],times[j],args...)
-        end
-    end
-    if norm
-        normalize!(state)
-    end
-    return state
-end
 
-function get_waveguide_location(b::WaveguideBasis)
-    return 1
-end
+"""
+    view_waveguide(ψ::ket)
+    view_waveguide(ψ::ket,index)
 
-function get_waveguide_location(b::CompositeBasis)
-    return findall(x->typeof(x)<:WaveguideBasis || typeof(x)<:OneTimeBasis,b.bases)[1]
-end
+View the Waveguide state given a state ψ containing a WaveguideBasis by returning `view(reshape(ψ.data,Tuple(ψ.basis.shape)),index...)`. If no index is provided the ground state is returned.
+The index provided should be of the form `[:,i,j]` where `(:)` is at the location of the WaveguideBasis and i and j are indeces of other basises. See example: 
 
+```
+times=0:0.1:10
+bw = WaveguideBasis(2,times)
+bc1 = FockBasis(2)
+bc2 = FockBasis(2)
+ψ_waveguide = onephoton(bw,x->1)
+ψ_total = ψ_waveguide ⊗ fockstate(bc1,1) ⊗ fockstate(bc2,1)
+ψ_view = view_waveguide(ψ_total)
+ψ_view_index = view_waveguide(ψ_total,[:,1,1])
+ψ_view==ψ_view_index
+```
+"""
 function view_waveguide(ψ::Ket)
     loc = get_waveguide_location(ψ.basis)
     index = Tuple(i==loc[1] ? (:) : 1 for i in 1:length(ψ.basis.shape))
     view_waveguide(ψ,index)
 end
-
 function view_waveguide(ψ::Ket,index)
     view(reshape(ψ.data,Tuple(ψ.basis.shape)),index...)
 end
 
+
+"""
+    view_onephoton(ψ::Ket)
+    view_onephoton(ψ::Ket,index)
+
+Return a view of the onephoton mode ``ξ(t)`` given an input state containing a onephoton waveguide state: ``\\int_{t_0}^{t_{end}} dt  ξ(t) w^†(t) |0⟩``
+If no index is provided the ground state is returned. Index should follow same form outlined in [`view_waveguide`](@ref).
+
+TO DO: PERHAPS CHANGE NAME?
+
+"""
 function view_onephoton(ψ::Ket)
     viewed_data = view_waveguide(ψ::Ket)
     nsteps = get_nsteps(ψ.basis)
     return  view(viewed_data,2:nsteps+1)
 end
-
 function view_onephoton(ψ::Ket,index)
     viewed_data = view_waveguide(ψ::Ket,index)
     nsteps = get_nsteps(ψ.basis)
     return  view(viewed_data,2:nsteps+1)
 end
 
+
+"""
+    view_twophoton(ψ::Ket)
+    view_twophoton(ψ::Ket,index)
+
+Return a view of the twophoton mode ``ξ(t_1,t_2)`` given an input state containing a twophoton waveguide state: ``\\int_{t_0}^{t_{end}} dt' \\int_{t_0}^{t_{end}} dt  ξ(t,t') w^†(t)w^†(t') |0⟩``
+If no index is provided the ground state is returned. Index should follow same form outlined in [`view_waveguide`](@ref).
+
+TO DO: PERHAPS CHANGE NAME?
+
+"""
 function view_twophoton(ψ::Ket)
     loc = get_waveguide_location(ψ.basis)
     index = Tuple(i==loc[1] ? (:) : 1 for i in 1:length(ψ.basis.shape))
     view_twophoton(ψ::Ket,index)
 end
-
-
-
 function view_twophoton(ψ::Ket,index)
     viewed_data = view_waveguide(ψ::Ket,index)
     nsteps = get_nsteps(ψ.basis)
@@ -128,17 +164,36 @@ function view_twophoton(ψ::Ket,index)
 end
 
 
+"""
+    get_waveguide_location(basis::WaveguideBasis)
+    get_waveguide_location(basis::CompositeBasis)
 
+Return index of [`WaveguideBasis`](@ref) location in Hilbert space of basis b. `Btotal = BW ⊗ BC` where BW is a [`WaveguideBasis`](@ref) and `BC` some other basis then `get_waveguide_location(Btotal)` returns 1. 
+While `Btotal = BC ⊗ BW` with `get_waveguide_location(Btotal)` returns 2.
+
+"""
+function get_waveguide_location(basis::WaveguideBasis)
+    return 1
+end
+function get_waveguide_location(basis::CompositeBasis)
+    return findall(x->typeof(x)<:WaveguideBasis,b.bases)[1]
+end
+
+
+"""
+    get_nsteps(basis::WaveguideBasis)
+    get_nsteps(basis::Basis)
+    get_nsteps(basis::CompositeBasis)
+
+Return nsteps of [`WaveguideBasis`](@ref) given either a [`WaveguideBasis`](@ref) or a [`CompositeBasis`](@ref) containing a [`WaveguideBasis`](@ref)
+
+"""
 function get_nsteps(basis::WaveguideBasis)
     basis.nsteps
 end
-
-
-
 function get_nsteps(basis::Basis)
     0
 end
-
 function get_nsteps(basis::CompositeBasis)
     for b in basis.bases
         if get_nsteps(b) != 0
@@ -148,14 +203,20 @@ function get_nsteps(basis::CompositeBasis)
 end
 
 
+"""
+    get_waveguidetimeindex(basis::WaveguideBasis)
+    get_waveguidetimeindex(basis::Basis)
+    get_waveguidetimeindex(basis::CompositeBasis)
+
+Return timeindex of [`WaveguideBasis`](@ref) given either a [`WaveguideBasis`](@ref) or a [`CompositeBasis`](@ref) containing a [`WaveguideBasis`](@ref)
+
+"""
 function get_waveguidetimeindex(basis::WaveguideBasis)
     basis.timeindex
 end
-
 function get_waveguidetimeindex(basis::Basis)
     0
 end
-
 function get_waveguidetimeindex(basis::CompositeBasis)
     for b in basis.bases
         if get_waveguidetimeindex(b) != 0

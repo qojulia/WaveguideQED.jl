83edf5ad14b5c993f0c5d25558547b86c2b36405
Matias Bundgaard-Nielsen <56378038+mabuni1998@users.noreply.github.com>
2024-04-18 17:31:24 +0200

    Updated documentation of expect_waveguide

--- src/WaveguideOperator.jl
+++ src/WaveguideOperator.jl
@@ -181,9 +181,9 @@ function identityoperator(::Type{T},::Type{ComplexF64}, b1::Basis, b2::Basis) wh
 end
 
 """
-    expect_waveguide(O, psi,times)
+    expect_waveguide(O, psi)
 
-Returns the sum of expectation values of O over all times in times. This is usefull to extract information from waveguide states.
+Returns the sum of expectation values of O over waveguide time indeces. This is usefull to extract information from waveguide states.
 For example the number of photons can be computed as: `expect_waveguide(create(bw)*destroy(bw), psi, times)`, where BW is the waveguidebasis. Mathematically this is equivalent to ``sum_n \\langle \\psi | O(t_n) | \\psi \\rangle``.
 For ``O(t_k) = w_k^\\dagger w_k`` acting on a single photon state ``|\\psi \\rangle = \\sum_k \\xi(t_k) w_k^\\dagger |0\\rangle``, one would therefore have: ``\\langle O \\rangle = \\sum_k |\\xi(t_k)|^2 = 1``, where the last follows from ``\\xi(t)`` being normalized.
 """

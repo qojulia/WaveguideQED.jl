994d07efd9b480bd8569c36d13f6b5e8bfe1531b
Matias Bundgaard-Nielsen <56378038+mabuni1998@users.noreply.github.com>
2025-03-24 09:38:31 +0100

    import mul! in WaveguideQEDExt
    To ensure that mul! gets properly disaptched in GPU

--- ext/WaveguideQEDGPUExt/WaveguideQEDGPUExt.jl
+++ ext/WaveguideQEDGPUExt/WaveguideQEDGPUExt.jl
@@ -4,8 +4,10 @@ using WaveguideQED
 import WaveguideQED: _is_destroy,_is_create,_is_identity,twophoton_index
 using CUDA
 using QuantumOpticsBase
-import QuantumOpticsBase: _tp_matmul_first!,_tp_matmul_last!
 using Strided
+import QuantumOpticsBase: _tp_matmul_first!,_tp_matmul_last!
+import LinearAlgebra: mul!
+
 
 include("gpu_waveguideoperator.jl")
 include("gpu_cavitywaveguideoperator.jl")
